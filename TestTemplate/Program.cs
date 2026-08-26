ef80ddd4b261836d2c2b4cec96f658d7245d3f83
thanhthuy1003 <lethithanhthuy102003@gmail.com>
2026-08-26 21:46:47 +0700

    refactor(template): update Adaptive Card template structure and data handling for order line items

--- TestTemplate/Program.cs
+++ TestTemplate/Program.cs
@@ -1,18 +1,17 @@
 using System;
+using System.Collections.Generic;
 using AdaptiveCards.Templating;
+
+public record ConfirmCreateOrderLine(string Material, decimal Qty, string Plant = """", string Unit = """");
+
 class Program {
     static void Main() {
-        var templateJson = @"{
-            ""type"": ""AdaptiveCard"",
-            ""body"": [
-                {
-                    ""type"": ""TextBlock"",
-                    ""text"": ""Plant: ${Plant}  Unit: ${Unit}""
-                }
-            ]
-        }";
+        var templateJson = ""{ \""type\"": \""AdaptiveCard\"", \""body\"": [ { \""type\"": \""FactSet\"", \""facts\"": [ { \""$data\"": \""${lineItems}\"", \""title\"": \""${Material}\"", \""value\"": \""Qty ${Qty}  Plant: ${Plant}  Unit: ${Unit}\"" } ] } ] }"";
         var template = new AdaptiveCardTemplate(templateJson);
-        var data = new { Plant = """", Unit = (string)null };
+        var lineItems = new List<ConfirmCreateOrderLine> {
+            new ConfirmCreateOrderLine(""123"", 5, """", """")
+        };
+        var data = new { lineItems = lineItems };
         Console.WriteLine(template.Expand(data));
     }
 }
